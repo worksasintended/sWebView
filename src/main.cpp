31e6c0b99a7e0a10c39458f1e20286a72c0c9e58
worksasintended <marc@marschalls.net>
2018-02-21 16:04:56 +0100

    add structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,16 @@ using namespace Wt;
 using namespace std;
 
 
-
-
-
+std::unique_ptr<WApplication> createApplication(const Wt::WEnvironment& env){
+  return make_unique<InfoApplication>(env);
+}
 
 int main(int argc, char **argv){
   try {
     Wt::WServer server(argc, argv, WTHTTP_CONFIGURATION);
     server.addEntryPoint(Wt::EntryPointType::Application, createApplication);
 
-    Session::configureAuth();
+//    Session::configureAuth();
 
     server.run();
   } catch (Wt::WServer::Exception& e) {
