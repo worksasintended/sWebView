e5907e4f41d6b4e23f97a6230ef35ed5a3a56349
worksasintended <marc@marschalls.net>
2018-02-21 16:13:34 +0100

    add RootApp

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,29 @@
 using namespace Wt;
 using namespace std;
 
+class RootApplication : public Wt::WApplication{
+  public: 
+    RootApplication(const Wt::WEnvironment& env) : Wt::WApplication(env){
+      auto bootstrapTheme = make_shared<Wt::WBootstrapTheme>();
+      bootstrapTheme->setVersion(Wt::BootstrapVersion::v3);
+      bootstrapTheme->setResponsive(true);
+      setTheme(bootstrapTheme);
+      useStyleSheet("resources/themes/bootstrap/3/bootstrap-theme.min.css");
+      auto layout = root()->setLayout(make_unique<WBorderLayout>());
+      auto container = layout->addWidget( make_unique<WContainerWidget>(), Wt::LayoutPosition::North );
+      auto image = container->addWidget( make_unique<Wt::WImage>(Wt::WLink("banner.png")) );
+    }
+
+}
 
 std::unique_ptr<WApplication> createApplication(const Wt::WEnvironment& env){
-  return make_unique<InfoApplication>(env);
+  return make_unique<RootApplication>(env);
 }
 
 int main(int argc, char **argv){
   try {
     Wt::WServer server(argc, argv, WTHTTP_CONFIGURATION);
     server.addEntryPoint(Wt::EntryPointType::Application, createApplication);
-
-//    Session::configureAuth();
 
     server.run();
   } catch (Wt::WServer::Exception& e) {
