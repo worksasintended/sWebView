5a7a32d430f9d09b9d0712f87c6d75aebab8de2f
worksasintended <marc@marschalls.net>
2018-02-21 17:39:12 +0100

    added basic framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <Wt/WApplication.h>
+#include <Wt/WContainerWidget.h>
 #include <Wt/WServer.h>
+#include <Wt/WBootstrapTheme.h>
+#include <Wt/WBorderLayout.h>
+#include <Wt/WImage.h>
+#include <Wt/WText.h>
+#include <Wt/WMenu.h>
+#include <Wt/WNavigationBar.h>
+#include <Wt/WStackedWidget.h>
+#include <string>
+
+
 
 using namespace Wt;
 using namespace std;
@@ -7,6 +18,8 @@ using namespace std;
 class RootApplication : public Wt::WApplication{
   public: 
     RootApplication(const Wt::WEnvironment& env) : Wt::WApplication(env){
+      
+      //basic layout
       auto bootstrapTheme = make_shared<Wt::WBootstrapTheme>();
       bootstrapTheme->setVersion(Wt::BootstrapVersion::v3);
       bootstrapTheme->setResponsive(true);
@@ -15,9 +28,19 @@ class RootApplication : public Wt::WApplication{
       auto layout = root()->setLayout(make_unique<WBorderLayout>());
       auto container = layout->addWidget( make_unique<WContainerWidget>(), Wt::LayoutPosition::North );
       auto image = container->addWidget( make_unique<Wt::WImage>(Wt::WLink("banner.png")) );
+      
+      //navigation bar
+      Wt::WNavigationBar *navigation = container->addWidget(make_unique<Wt::WNavigationBar>());
+      navigation->setResponsive(true);
+      Wt::WStackedWidget *contentsStack = container->addWidget(Wt::cpp14::make_unique<Wt::WStackedWidget>());
+      contentsStack->addStyleClass("contents");
+      auto leftMenu = Wt::cpp14::make_unique<Wt::WMenu>(contentsStack);
+      auto leftMenu_ = navigation->addMenu(std::move(leftMenu));
+      leftMenu_->addItem("Information", make_unique<Wt::WText>("Information part, visible without authentification"));
+      leftMenu_->addItem("Administration", make_unique<Wt::WText>("Administration part, authentification required"));
     }
 
-}
+};
 
 std::unique_ptr<WApplication> createApplication(const Wt::WEnvironment& env){
   return make_unique<RootApplication>(env);
