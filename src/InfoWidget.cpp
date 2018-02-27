cb6e9aa158a9d7000020026eaea80985108e8bb9
Stefan Kemnitz <kemnitz.stefan@gmail.com>
2018-02-27 16:04:07 +0100

    example of a data fetch implementation with the slurm api

--- src/InfoWidget.cpp
+++ src/InfoWidget.cpp
@@ -1,25 +1,20 @@
 #include <Wt/WText.h>
 #include <Wt/WPushButton.h>
-#include <slurm/slurm.h>
 #include "InfoWidget.hpp"
 
 using namespace std;
 using namespace Wt;
 
+
 InfoWidget::InfoWidget(){
-  this->addWidget(make_unique<Wt::WText>("This is the information widget", Wt::TextFormat::Plain));
-  auto update = this->addWidget(make_unique<WPushButton>("update") );
-  update->clicked().connect(
-    [=](){
-      partition_info_msg_t* info;
 
-      time_t now = time(NULL);
-      now = now - (60 * 60 * 24);
+  // first create something we can track with the update function
+  partitions_info_data = new PartitionsInfoData();
+  partitions_info_data->add_observer(this);
+  partitions_info_data->update_data();
 
-      slurm_load_partitions(now, &info, SHOW_FEDERATION);
-      slurm_print_partition_info_msg( stdout, info, 0 );
-    }
-  );
+  // call the routine that draws all elements to this widget
+  update();
 }
 
 
@@ -27,3 +22,20 @@ InfoWidget::~InfoWidget() {
 
 }
 
+void InfoWidget::update(){
+  this->clear();
+  this->addWidget(make_unique<Wt::WText>("This is the information widget", Wt::TextFormat::Plain));
+
+  auto update = this->addWidget(make_unique<WPushButton>("update") );
+  update->clicked().connect(
+    [=](){
+      partitions_info_data->update_data();
+    }
+  );
+
+  // now add some elements that display the information from the infered slurm data
+  for (int i = 0; i < partitions_info_data->number_of_partitions(); ++i){
+    this->addWidget( make_unique<WText>(partitions_info_data->get_partition_name(i) ) );
+  }
+
+}
--- src/InfoWidget.hpp
+++ src/InfoWidget.hpp
@@ -1,10 +1,44 @@
 #pragma once
 #include <Wt/WContainerWidget.h>
+#include <slurm/slurm.h>
+
+#include "Observable.hpp"
 
 
-class InfoWidget : public Wt::WContainerWidget {
+// facade class to infer data from slurm that is shown multiple times in the gui
+//
+struct PartitionsInfoData : public Observable {
+  public:
+    void update_data(){
+      if ( info ) {
+        //clean up memory
+        free(info);
+      }
+      time_t now = time(NULL) - 60*60*24; // does not work with something lower than this ????? but why 
+      int ret = slurm_load_partitions(now, &info, SHOW_FEDERATION);
+    }
+
+    auto number_of_partitions(){
+      return info->record_count;
+    }
+   
+    // one could create a PartitionInfoData class ("NOTE without the s") to manage the 
+    // information of a single partition but this could be overkill 
+    std::string get_partition_name( int idx ) {
+      return info->partition_array[idx].name;
+    } 
+
+  private:
+    partition_info_msg_t* info = nullptr;
+};
+
+class InfoWidget : public Wt::WContainerWidget, public Observer {
 public:
     InfoWidget ();
     virtual ~InfoWidget ();
+    virtual void update() override;
+
+private:
+    PartitionsInfoData* partitions_info_data = nullptr; // perhaps make this a shared_ptr
 };
 
