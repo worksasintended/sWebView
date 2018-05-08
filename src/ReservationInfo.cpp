dfeaea36af7abb903d2d399c2ed8ce8918163972
soeren <soeren@soeren.com>
2018-05-08 23:40:47 +0200

    Added RegexSearch to Reservations

--- src/ReservationInfo.cpp
+++ src/ReservationInfo.cpp
@@ -1,6 +1,6 @@
 
 #include "ReservationInfo.hpp"
-
+#include <ctime>
 #include <slurm/slurm.h>
 
 ReservationInfo::ReservationInfo( reserve_info_t* _info ) {
@@ -13,4 +13,22 @@ std::string ReservationInfo::get_name(){
   return info->name;
 }
 
-
+std::string ReservationInfo::get_start_time_string(){
+	time_t time_t_var = info->end_time;
+	std::string start_time_string;
+  if ( time_t_var > 0 ) {
+      start_time_string = std::ctime(&time_t_var);
+  }
+  return start_time_string;
+}
+std::string ReservationInfo::get_end_time_string(){
+	time_t time_t_var = info->end_time;
+	std::string end_time_string;
+  if ( time_t_var > 0 ) {
+      end_time_string = std::ctime(&time_t_var);
+  }
+  return end_time_string;
+}
+std::string ReservationInfo::get_users(){
+	return info->users;
+}
